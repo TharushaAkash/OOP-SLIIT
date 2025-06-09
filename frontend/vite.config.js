709cc804722b684661aa17e8e359808a4db3f0e3
Tharusha Akash <tharushaakasha22@gmail.com>
2025-06-09 11:24:28 +0530

    Delete frontend directory

--- frontend/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/OOP-SLIIT/', // For GitHub Pages
-});
